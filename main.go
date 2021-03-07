e836c0ac8f11fec49053812da08dc65f62b8f476
reski-rukmantiyo <reski.rukmantiyo@gmail.com>
2021-03-07 16:15:05 +0700

    added create table and columns

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"absensi/config"
 	"absensi/tools"
+	"errors"
 	"log"
 	"net/url"
+	"os"
 	"time"
 
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -21,11 +24,37 @@ type TokenResult struct {
 
 type Daftar struct {
 	gorm.Model
-	WorkData          time.Time `gorm:"uniqueIndex"`
-	LoginDateTime     time.Time
-	LogoutDateTime    time.Time
+	WorkingDate       time.Time `gorm:"column:working_date;uniqueIndex"`
+	LoginDateTime     time.Time `gorm:"column:login_date"`
+	LogoutDateTime    time.Time `gorm:"column:logout_date"`
 	LoginExecuteTime  time.Time
 	LogoutExecuteTime time.Time
+}
+
+func checkAndCreateSchedule(filePath string) (*Daftar, error) {
+	db, err := gorm.Open(sqlite.Open(filePath), &gorm.Config{})
+	if err != nil {
+		log.Panic("failed to connect database")
+		return nil, err
+	}
+	daftar := &Daftar{}
+	//db.First(&daftar, "workingDate=?", time.Now().Format("2006-01-02")).Error
+	result := db.Where("working_date=?", time.Now().Format("2006-01-02")).First(&daftar)
+	if err = result.Error; errors.Is(err, gorm.ErrRecordNotFound) {
+		// Create another record
+		//log.Panicf("Create new record. %s", err.Error())
+	}
+	return daftar, nil
+}
+
+func createFile(filePath string) error {
+	db, err := gorm.Open(sqlite.Open(filePath), &gorm.Config{})
+	if err != nil {
+		log.Panic("failed to connect database")
+		return err
+	}
+	db.AutoMigrate(&Daftar{})
+	return nil
 }
 
 func main() {
@@ -33,12 +62,24 @@ func main() {
 	// token := Login(config)
 	// log.Println(token)
 
-	// db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-	// if err != nil {
-	// 	log.Panic("failed to connect database")
-	// }
-	// db.AutoMigrate(&Daftar{})
-
+	home, err := os.UserHomeDir()
+	folderPath := home + "/" + ".absensi"
+	fileName := folderPath + "/" + "absensi.db"
+	_, err = os.Stat(folderPath)
+	if err != nil {
+		err = os.Mkdir(folderPath, os.ModePerm)
+		if err != nil {
+			log.Panic("Create File Error. Panic and abort the apps")
+		}
+	}
+	_, err = os.Stat(fileName)
+	if err != nil {
+		err = createFile(fileName)
+		if err != nil {
+			log.Panic("Create File Error. Panic and abort the apps")
+		}
+	}
+	checkAndCreateSchedule(fileName)
 }
 
 func Login(config *config.Config) string {
