61e900a6e85f58874e4a3922c96ff51ec5f79a03
Reski Rukmantiyo <reski.rukmantio@lintasarta.co.id>
2021-03-18 08:08:17 +0700

    add function for copy and check file

--- source/tools/path.go
+++ source/tools/path.go
@@ -1,6 +1,8 @@
 package tools
 
 import (
+	"errors"
+	"io"
 	"os"
 )
 
@@ -10,4 +12,35 @@ func GetHomeDirectory() (string, error) {
 		return "", err
 	}
 	return home, nil
+}
+
+func CheckFileExists(src string) bool {
+	if _, err := os.Stat(src); err == nil {
+		return true
+	} else {
+		return false
+	}
+}
+
+func Copy(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, errors.New(src + " is not a regular file")
+	}
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
 }
